cba273a6feff0b49704c7906886fef40f7563517
Will Rose <77295571+w1123581321345589@users.noreply.github.com>
2026-03-18 09:31:04 -0400

    v0.22.0: tests/test_e2e.py

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -815,7 +815,7 @@ class TestVersionConsistency:
     should match.
     """
 
-    EXPECTED_VERSION = "0.21.0"
+    EXPECTED_VERSION = "0.22.0"
 
     def test_module_version(self):
         assert aiglos.__version__ == self.EXPECTED_VERSION, (
