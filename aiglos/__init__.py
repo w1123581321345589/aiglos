51e93214d181e2f82e4590df7c8b8bfa2e79768f
Will Rose <77295571+w1123581321345589@users.noreply.github.com>
2026-03-18 09:30:45 -0400

    v0.22.0: aiglos/__init__.py

--- aiglos/__init__.py
+++ aiglos/__init__.py
@@ -49,7 +49,7 @@ try:
         raise ValueError("stale")
     __version__: str = _v
 except Exception:
-    __version__ = "0.21.0"  # canonical version for this release
+    __version__ = "0.22.0"  # canonical version for this release
 __author__  = "Aiglos"
 __email__   = "will@aiglos.io"
 __license__ = "MIT"
